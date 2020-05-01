96b674783c773c26e88ff9cf5407bfe7339cd2a4
fromzeya@gmail.com <fromzeya@mail.ru>
2020-05-01 15:14:37 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello to react!
+          Hello to react again!
         </p>
         <a
           className="App-link"
